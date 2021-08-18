d0fb83a5a74937b9dbbcc17100243b3ed616a7a1
李春鹏 <lichunpeng@chinau.cc>
2021-08-18 11:39:33 +0800

    FIXED 登录联调

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -81,7 +81,7 @@ export default{
         }
     },
     created() {
-        // this.getImgCode();
+        this.getImgCode();
         // 回车键登录
         let that = this;
         document.onkeydown = function(e) {
@@ -99,6 +99,7 @@ export default{
          // 获取图形验证码
         getImgCode() {
             getImageCode().then(res => {
+                console.log(res);
                 this.imgCode = res.imageCode;
                 this.uuid = res.uuid;
             });
